aa04cd060a5e846fae3941f720f31b30ded6611f
Kabir Maan <kabirmaan9@gmail.com>
2019-10-14 08:04:33 +0100

    Added Storybook and CircleCi config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loaders: [
+      require.resolve("awesome-typescript-loader"),
+      require.resolve("react-docgen-typescript-loader")
+      // require.resolve("@storybook/source-loader")
+    ],
+    enforce: "pre"
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
+  return config;
+};
